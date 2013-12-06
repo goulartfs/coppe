b0ccdc98ad65e7ed1dc912632daa2eb6f01aa4c4
goulartfs <goulartfs@gmail.com>
2013-12-06 16:10:40 -0200

    Inicio do layout parque

---
+++ layout/parque/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>COPPE - Parque</title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+        <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="../assets/js/html5shiv.js"></script>
+        <![endif]-->
+
+        <!-- Fav and touch icons -->
+        <link rel="shortcut icon" href="">
+    </head>
+
+    <body>
+        <div id="header">
+            <div class="header-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="span5 offset11 header-top-links">
+                            <a href="#" title="Acesso à informação">
+                                <img src="./images/acesso-informacao.jpg" alt="Acesso à informação" />
+                            </a>
+                            <a href="#" title="Brasil">
+                                <img src="./images/brasil.jpg" alt="Brasil" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-middle">
+                <div class="container">
+                    <div class="row">
+                        <div class="span16">
+                            <img src="./images/logo.jpg" alt="Parque Tecnológico UFRJ"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-navbar">
+                <div class="container">
+                    <div class="row">
+                        <div class="span16">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
