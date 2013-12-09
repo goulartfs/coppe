75cd97ea43ff4874545dc65e0ed65dc1fbde5d84
Filipe Synthis <goulartfs@gmail.com>
2013-12-08 23:23:08 -0200

    Layout

--- layout/parque/index.php
+++ layout/parque/index.php
@@ -38,8 +38,40 @@
             <div class="header-middle">
                 <div class="container">
                     <div class="row">
-                        <div class="span16">
+                        <div class="span6">
                             <img src="./images/logo.jpg" alt="Parque Tecnológico UFRJ"/>
+                        </div>
+                        <div class="span10 text-right">
+                            <div class="row">
+                                <div class="span10">
+                                    <ul class="top-links inline">
+                                        <li class="incubadora"><a href='#' title="Incubadora de Empresas">Incubadora de Empresas</a></li>
+                                        <li class="ufrj"><a href='#' title="UFRJ">UFRJ</a></li>
+                                        <li class="english"><a href='#' title="English Version">English Version</a></li>
+                                    </ul>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="span10 search-bar">
+                                    <form class="form-inline" action="#" method="get">
+                                        <div class="input-prepend input-append">
+                                            <label class="add-on" for="input-search">Busca</label>
+                                            <input id="input-search" class="span4" type="text" name="s"/>
+                                            <button class="btn" type="button">OK</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="span10">
+                                    <ul class="bottom-links inline">
+                                        <li><a href='#' title="Localização">Localização</a></li>
+                                        <li><a href='#' title="Contato">Contato</a></li>
+                                        <li><a href='#' title="Área restrita">Área restrita</a></li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -79,19 +111,39 @@
                 <div class="content">
                     <div class="container">
                         <div class="row">
-                            <div class="span16">
+                            <div class="span12">
                                 <p>
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                                 </p>
                                 <p>
                                     Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
                                 </p>
+                                <h2>Lorem ipsum dolor sit amet</h2>
+                                <h3>Lorem ipsum dolor sit amet</h3>
+                                <h4>Lorem ipsum dolor sit amet</h4>
+                                <h5>Lorem ipsum dolor sit amet</h5>
+                                <h6>Lorem ipsum dolor sit amet</h6>
                                 <p>
                                     Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst. Vivamus facilisis diam at odio. Mauris dictum, nisi eget consequat elementum, lacus ligula molestie metus, non feugiat orci magna ac sem. Donec turpis. Donec vitae metus. Morbi tristique neque eu mauris. Quisque gravida ipsum non sapien. Proin turpis lacus, scelerisque vitae, elementum at, lobortis ac, quam. Aliquam dictum eleifend risus. In hac habitasse platea dictumst. Etiam sit amet diam. Suspendisse odio. Suspendisse nunc. In semper bibendum libero.
                                 </p>
                                 <p>
                                     Proin nonummy, lacus eget pulvinar lacinia, pede felis dignissim leo, vitae tristique magna lacus sit amet eros. Nullam ornare. Praesent odio ligula, dapibus sed, tincidunt eget, dictum ac, nibh. Nam quis lacus. Nunc eleifend molestie velit. Morbi lobortis quam eu velit. Donec euismod vestibulum massa. Donec non lectus. Aliquam commodo lacus sit amet nulla. Cras dignissim elit et augue. Nullam non diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Aenean vestibulum. Sed lobortis elit quis lectus. Nunc sed lacus at augue bibendum dapibus.
                                 </p>
+                            </div>
+                            <div id="sidebar" class="span4">
+                                <div class="sidebar">
+                                    <h2>Notícias do Parque</h2>
+                                    <div class="widget">
+                                        <span class="sidebar-title">Últimas Notícias</span>
+                                        <ul>
+                                            <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing</a></li>
+                                            <li><a href="#">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="more text-right">
+                                        <a href="#">[+] Mais</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -103,13 +155,13 @@
                                 <form class="form-inline" action="#" method="post">
                                     <div class="input-prepend input-append">
                                         <label class="add-on" for="input-email">Receba nossa newsletter</label>
-                                        <input id="input-email" class="span3" type="text" name="email"/>
+                                        <input id="input-email" class="span6" type="text" name="email" placeholder="Ex: email@email.com.br"/>
                                         <button class="btn" type="button">Assinar</button>
                                     </div>
                                 </form>
                             </div>
-                            <div class="span4">
-
+                            <div class="span4 text-right">
+                                <img src="images/links-sociais.jpg" alt="Redes Sociais"/>
                             </div>
                         </div>
                     </div>
