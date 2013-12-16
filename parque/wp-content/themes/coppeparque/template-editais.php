478cd3f4e725ddcda3cdca8b67cb47183e2ee9fa
goulartfs <goulartfs@gmail.com>
2013-12-16 19:57:03 -0200

    Ajustes em css, adição de modelo de página para Editais.

---
+++ parque/wp-content/themes/coppeparque/template-editais.php
@@ -0,0 +1,114 @@
+<?php /* Template Name: Editais */ ?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+get_header();
+?>
+<div id="content">
+<?php if (!is_home() && !is_search()) { ?>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php if (is_page() ) : ?>
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row">
+                            <div class="span16">
+                                <h1><?php the_title() ?></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <div class="content-bread">
+                <div class="container">
+                    <div class="row">
+                        <div class="span16">
+                            <div class="breadcrumb">
+                                <?php
+                                if(function_exists('bcn_display'))
+                                {
+                                    bcn_display();
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+<?php } ?>
+    <div class="content">
+    <div class="container">
+    <div class="row">
+    <div class="span12">
+        <div class="row">
+            <div class="span12">
+                <?php if (have_posts()) : ?>
+
+                    <?php /* The loop */ ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <?php get_template_part('content', get_post_format()); ?>
+                    <?php endwhile; ?>
+
+                    <?php coppeparque_paging_nav(); ?>
+
+                <?php else : ?>
+                    <?php get_template_part('content', 'none'); ?>
+                <?php endif; ?>
+                <div>
+                    <?php
+                    // The Query
+                    $args = array('category_name' => 'editais', 'posts_per_page' => 10);
+                    $loop = new WP_Query($args);
+
+                    // User Loop
+                    ?>
+                    <?php if ($loop->have_posts()) : ?>
+
+                        <?php /* The loop */ ?>
+                        <div class="edital">
+                            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                                <h2><?php the_title() ?></h2>
+                                <?php if(get_field('data_inicio') || get_field('data_fim')) { ?>
+                                    <div class="pre-content">
+                                        <?php if(get_field('data_inicio')) { ?>
+                                            <strong>Data início: </strong> <?php print date('d/m/Y', strtotime(get_field('data_inicio'))) ?>
+                                        <?php } ?>
+                                        &NonBreakingSpace;|&NonBreakingSpace;
+                                        <?php if(get_field('data_fim')) { ?>
+                                            <strong>Data fim: </strong> <?php print date('d/m/Y', strtotime(get_field('data_fim'))) ?>
+                                        <?php } ?>
+                                    </div>
+                                <?php } ?>
+                                <div class="edital-content">
+                                    <?php the_content() ?>
+                                </div>
+                            <?php endwhile; ?>
+                        </div>
+
+                        <?php coppeparque_paging_nav(); ?>
+
+                    <?php else : ?>
+                        <?php //get_template_part('content', 'none'); ?>
+                    <?php endif; ?>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
