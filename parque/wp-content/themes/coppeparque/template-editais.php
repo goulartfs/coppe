ca9a0c65e5fc17549ac23d934677cc517c1c3edc
Filipe Synthis <goulartfs@gmail.com>
2013-12-27 11:43:15 -0200

    Ajustes em editais. Exibe somente editais ativos. Adição de FLitflout em 'leia mais'

--- parque/wp-content/themes/coppeparque/template-editais.php
+++ parque/wp-content/themes/coppeparque/template-editais.php
@@ -20,7 +20,7 @@ get_header();
 <?php if (!is_home() && !is_search()) { ?>
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
-            <?php if (is_page() ) : ?>
+            <?php if (is_page()) : ?>
                 <div class="content-header">
                     <div class="container">
                         <div class="row">
@@ -37,8 +37,7 @@ get_header();
                         <div class="span16">
                             <div class="breadcrumb">
                                 <?php
-                                if(function_exists('bcn_display'))
-                                {
+                                if (function_exists('bcn_display')) {
                                     bcn_display();
                                 }
                                 ?>
@@ -71,7 +70,22 @@ get_header();
                 <div>
                     <?php
                     // The Query
-                    $args = array('category_name' => 'editais', 'posts_per_page' => 10);
+                    $args = array('category_name' => 'editais', 'posts_per_page' => 10,
+                        'meta_query' => array(
+                            'relation' => 'AND',
+                            array(
+                                'key' => 'data_inicio',
+                                'value' => date('Ymd'),
+                                'type' => 'date',
+                                'compare' => '<='
+                            ),array(
+                                'key' => 'data_fim',
+                                'value' => date('Ymd'),
+                                'type' => 'date',
+                                'compare' => '>='
+                            )
+                        )
+                    );
                     $loop = new WP_Query($args);
 
                     // User Loop
@@ -81,20 +95,35 @@ get_header();
                         <?php /* The loop */ ?>
                         <div class="edital">
                             <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-                                <h2><?php the_title() ?></h2>
-                                <?php if(get_field('data_inicio') || get_field('data_fim')) { ?>
-                                    <div class="pre-content">
-                                        <?php if(get_field('data_inicio')) { ?>
-                                            <strong>Data início: </strong> <?php print date('d/m/Y', strtotime(get_field('data_inicio'))) ?>
-                                        <?php } ?>
-                                        &NonBreakingSpace;|&NonBreakingSpace;
-                                        <?php if(get_field('data_fim')) { ?>
-                                            <strong>Data fim: </strong> <?php print date('d/m/Y', strtotime(get_field('data_fim'))) ?>
-                                        <?php } ?>
+                                <div class="edital-<?php the_ID() ?>">
+                                    <h2><?php the_title() ?></h2>
+                                    <?php if (get_field('data_inicio') || get_field('data_fim')) { ?>
+                                        <div class="pre-content">
+                                            <?php if (get_field('data_inicio')) { ?>
+                                                <strong>Data
+                                                    início: </strong> <?php print date('d/m/Y', strtotime(get_field('data_inicio'))) ?>
+                                            <?php } ?>
+                                            &NonBreakingSpace;|&NonBreakingSpace;
+                                            <?php if (get_field('data_fim')) { ?>
+                                                <strong>Data
+                                                    fim: </strong> <?php print date('d/m/Y', strtotime(get_field('data_fim'))) ?>
+                                            <?php } ?>
+                                        </div>
+                                    <?php } ?>
+                                    <?php
+                                    global $more;
+                                    $more = 0;
+                                    ?>
+                                    <div class="edital-content">
+                                        <?php the_content('Leia mais...'); ?>
                                     </div>
-                                <?php } ?>
-                                <div class="edital-content">
-                                    <?php the_content() ?>
+                                    <?php
+                                    global $more;
+                                    $more = 1;
+                                    ?>
+                                    <div class="pos-more">
+                                        <?php the_content('', true, ''); ?>
+                                    </div>
                                 </div>
                             <?php endwhile; ?>
                         </div>
@@ -102,13 +131,29 @@ get_header();
                         <?php coppeparque_paging_nav(); ?>
 
                     <?php else : ?>
-                        <?php //get_template_part('content', 'none'); ?>
+                        <div>
+                            <h4>Nenhum edital ativo no período atual.</h4>
+                        </div>
                     <?php endif; ?>
                 </div>
 
             </div>
         </div>
     </div>
+    <script>
+        jQuery(function () {
+            jQuery('.pos-more').hide();
+            jQuery('.more-link').click(function (e) {
+                e.preventDefault();
+                console.log(jQuery(this).parent('div[class|="edital"]'));
+                jQuery(this).toggleClass('active');
+                jQuery(this).parents('div[class|="edital"]').find('.pos-more').fadeToggle();
+            })
+        })
+    </script>
+    <style>
+
+    </style>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
