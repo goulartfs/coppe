1de9de50750d8b065fc3b2a2a6c28da909c96d62
Filipe Synthis <goulartfs@gmail.com>
2014-01-01 17:50:47 -0200

    Click em links da equipe.

--- parque/wp-content/themes/coppeparque/js/main.js
+++ parque/wp-content/themes/coppeparque/js/main.js
@@ -1,3 +1,8 @@
 jQuery(function(){
     jQuery('.disabled input').attr('readonly',true);
+
+    //Equipe
+    jQuery('.social_background a').click(function(){
+        window.open(jQuery(this).attr('href'));
+    })
 })
