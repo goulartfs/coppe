6b0b258dcb9cb3b68e6e67de77448e711a5fb0b8
Filipe Goulart <filipe.goulart@afilio.com.br>
2014-01-06 11:05:03 -0200

    Criação de página para biblioteca

---
+++ incubadora/wp-content/themes/coppeincubadora/template-biblioteca.php
@@ -0,0 +1,155 @@
+<?php /* Template Name: Biblioteca */ ?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+get_header();
+?>
+<div id="content">
+<?php if (!is_home() && !is_search()) { ?>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php if (is_page()) : ?>
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row">
+                            <div class="span16">
+                                <h1><?php the_title() ?></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <div class="content-bread">
+                <div class="container">
+                    <div class="row">
+                        <div class="span16">
+                            <div class="breadcrumb">
+                                <?php
+                                if (function_exists('bcn_display')) {
+                                    bcn_display();
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+<?php } ?>
+    <div class="content">
+    <div class="container">
+    <div class="row">
+    <div class="span12">
+        <div class="row">
+            <div class="span12 content-area">
+                <div class="wrapper">
+                    <?php if (have_posts()) : ?>
+
+                        <?php /* The loop */ ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <?php get_template_part('content', get_post_format()); ?>
+                        <?php endwhile; ?>
+
+                        <?php coppeincubadora_paging_nav(); ?>
+
+                    <?php else : ?>
+                        <?php get_template_part('content', 'none'); ?>
+                    <?php endif; ?>
+
+                    <?php
+                    $args = array('post_type' => 'biblioteca', 'posts_per_page' => 10);
+                    $loop = new WP_Query($args);
+                    ?>
+                    <?php if ($loop->have_posts()) : ?>
+
+                        <?php /* The loop */ ?>
+                        <div class="biblioteca-acordion">
+                            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                                <?php $terms = get_the_terms($post->id, 'tipo_artigo'); ?>
+                                <?php $terms = array_pop($terms) ?>
+                                <div class="row biblioteca-content">
+                                    <div class="span2 text-center biblioteca-img">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/images/docfile.jpg"/>
+                                    </div>
+                                    <div class="span10">
+                                        <h2 class="title"><?php the_title() ?><span class="tipo_artigo"><?php print $terms->name; ?></span></h2>
+                                        <div class="clearfix"></div>
+                                        <div class="row pos-info">
+                                            <div class="span3">
+                                            </div>
+                                            <div class="span7 text-right data-post">
+                                                <?php the_date('d/m/Y') ?>
+                                            </div>
+                                        </div>
+                                        <?php
+                                        global $more;
+                                        $more = 0;
+                                        ?>
+                                        <div class="edital-content">
+                                            <?php the_content('<span data-target="' . get_the_ID() . '">[+] MAIS</span>'); ?>
+                                            <?php if (get_field('link')) { ?>
+                                                <div class="link-externo">
+                                                    Acesse: <a href="<?php the_field('link') ?>" target="_blank"><?php the_field('link') ?></a>
+                                                </div>
+                                                <div id="link-mais-<?php the_ID() ?>">
+                                                </div>
+                                            <?php } ?>
+                                        </div>
+                                        <?php
+                                        global $more;
+                                        $more = 1;
+                                        ?>
+                                        <div class="pos-more" rel="<?php the_ID() ?>">
+                                            <?php the_content('', true, ''); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        </div>
+
+                        <?php coppeincubadora_paging_nav(); ?>
+
+                    <?php else : ?>
+                        <?php //get_template_part('content', 'none'); ?>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        jQuery(function () {
+            jQuery('a.more-link').each(function(){
+                clonado = jQuery(this).clone();
+                jQuery(this).remove();
+                clonado.appendTo("#link-mais-" + clonado.find('span').data('target'));
+            })
+
+            jQuery('.pos-more').hide();
+            jQuery('.more-link').click(function (e) {
+                e.preventDefault();
+                if (!jQuery(this).hasClass('active')) {
+                    jQuery('.pos-more').fadeOut();
+                    jQuery('.more-link').removeClass('active');
+                }
+                jQuery('.pos-more[rel=' + jQuery(this).find('span').data('target') + ']').fadeToggle();
+                jQuery(this).toggleClass('active');
+            })
+        })
+    </script>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
