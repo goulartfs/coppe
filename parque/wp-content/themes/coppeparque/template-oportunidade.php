545160d1c3aeb93817412926fd6698c0244cf108
Filipe Goulart <filipe.goulart@afilio.com.br>
2014-01-13 15:29:58 -0200

    Ajuste em Tela de Equipe

--- parque/wp-content/themes/coppeparque/template-oportunidade.php
+++ parque/wp-content/themes/coppeparque/template-oportunidade.php
@@ -146,8 +146,7 @@ get_header();
     </div>
     <script>
         jQuery(function () {
-            jQuery('.vaga-content').not(':first').hide();
-            jQuery('.vaga-item h2:first').addClass('active');
+            jQuery('.vaga-content').hide();
             jQuery('.vaga-item h2').click(function () {
                 jQuery(this).next('.vaga-block').find('.vaga-content').slideToggle();
                 jQuery(this).toggleClass('active');
