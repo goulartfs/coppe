a0ba8c4fd401333d251251545ecfd93dedeb4485
goulartfs <goulartfs@gmail.com>
2014-01-20 16:46:54 -0200

    novos templates

---
+++ incubadora/wp-content/themes/coppeincubadora/content-canal.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <?php if (has_post_thumbnail() && !post_password_required()) : ?>
+            <div class="entry-thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div>
+        <?php endif; ?>
+
+        <?php //if ( is_home() ) : ?>
+        <?php if (is_single()) : ?>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php else : ?>
+            <?php if (!is_page()) : ?>
+                <h1 class="entry-title">
+                    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                </h1>
+            <?php endif; ?>
+        <?php endif; // is_single() ?>
+        <?php //endif; // is_single() ?>
+
+
+        <div class="entry-meta">
+            <?php coppeincubadora_entry_meta(); ?>
+            <?php edit_post_link(__('Edit', 'coppeincubadora'), '<span class="edit-link">', '</span>'); ?>
+        </div><!-- .entry-meta -->
+    </header><!-- .entry-header -->
+
+    <?php if (is_search()) : // Only display Excerpts for Search ?>
+        <div class="entry-summary">
+            <?php the_excerpt(); ?>
+        </div><!-- .entry-summary -->
+    <?php else : ?>
+        <div class="entry-content">
+            <iframe width="100%" height="400px"
+                    src="//www.youtube.com/embed/<?php print getYoutubeIdFromUrl(get_field('video')); ?>?rel=0"
+                    frameborder="0" allowfullscreen></iframe><br><br>
+            <?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'coppeincubadora')); ?>
+            <?php wp_link_pages(array('before' => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'coppeincubadora') . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>')); ?>
+        </div><!-- .entry-content -->
+    <?php endif; ?>
+
+    <footer class="entry-meta">
+        <?php comments_template(); ?>
+    <?php if (comments_open() && !is_single()) : ?>
+                                <div class="comments-link">
+        <?php comments_popup_link('<span class="leave-reply">' . __('Leave a comment', 'coppeincubadora') . '</span>', __('One comment so far', 'coppeincubadora'), __('View all % comments', 'coppeincubadora')); ?>
+                                </div> .comments-link 
+    <?php endif; // comments_open() ?>
+    
+    <?php if (is_single() && get_the_author_meta('description') && is_multi_author()) : ?>
+        <?php get_template_part('author-bio'); ?>
+    <?php endif; ?>
+            </footer>
+    <!-- .entry-meta -->
+</article><!-- #post -->
---
+++ incubadora/wp-content/themes/coppeincubadora/single.php
@@ -0,0 +1,69 @@
+<?php
+get_header();
+?>
+<div id="content">
+<?php if (!is_home() && !is_search()) { ?>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php if (is_page() ) : ?>
+                <div class="content-header">
+                    <div class="container">
+                        <div class="row">
+                            <div class="span16">
+                                <h1><?php the_title() ?></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <div class="content-bread">
+                <div class="container">
+                    <div class="row">
+                        <div class="span16">
+                            <!--                                <ul class="breadcrumb">
+                                                                <li><a href="#">Home</a> <span class="divider">/</span></li>
+                                                                <li><a href="#">Library</a> <span class="divider">/</span></li>
+                                                                <li class="active">Data</li>
+                                                            </ul>-->
+                            <div class="breadcrumb">
+                                <?php
+                                if(function_exists('bcn_display'))
+                                {
+                                    bcn_display();
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+<?php } ?>
+    <div class="content">
+    <div class="container">
+    <div class="row">
+    <div class="span12">
+        <div class="row">
+            <div class="span12 content-area">
+                <div class="wrapper">
+
+                    <?php if (have_posts()) : ?>
+
+                        <?php /* The loop */ ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <?php get_template_part('content', get_post_format()); ?>
+                        <?php endwhile; ?>
+
+                        <?php coppeincubadora_paging_nav(); ?>
+
+                    <?php else : ?>
+                        <?php get_template_part('content', 'none'); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
